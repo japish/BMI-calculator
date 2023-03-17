cea2c569ff0196deffeb542f860d09e54298a4ba
japish <122783671+japish@users.noreply.github.com>
2023-03-17 14:29:16 +0530

    Create bmi_version1.py
    its the 1st version  of code

---
+++ bmi_version1.py
@@ -0,0 +1,13 @@
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+def get_bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Normal weight"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
